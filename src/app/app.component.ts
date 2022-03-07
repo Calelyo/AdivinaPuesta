1c59dea70f66db4f5c7e77b493f3dff09ec6b785
Calel Sprumont <calelyo@gmail.com>
2022-03-07 11:50:45 -0300

    Inicialización, Primer Componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,78 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'adivinapuesta';
+
+  aleatorio: number = this.numeroAleatorio();
+
+  oro: number = 0;
+
+  oculto: number = this.numeroAleatorio();
+
+  activado: boolean = true;
+
+  deuda: number = 0;
+
+  numeroAleatorio(): number{
+    let aleatorio = Math.round(Math.random()*10);
+
+    while(aleatorio < 1 || aleatorio > 10){
+      aleatorio = Math.round(Math.random()*10);
+    }
+    
+    return aleatorio;
+  }
+
+  activar(): void{
+    if(this.activado == false && this.oro >= 200){
+      this.oro -= 200;
+      this.activado = true;
+      this.aleatorio = this.numeroAleatorio();
+    }
+  }
+
+  desactivar(): void{
+    if(this.activado == true){
+      this.activado = false;
+    }
+  }
+
+  botonArriba(): void{
+    if(this.activado == true){
+      if(this.aleatorio < 10){
+        this.aleatorio += 1;
+      }
+    }
+  }
+
+  botonAbajo(): void{
+    if(this.activado == true){
+      if(this.aleatorio > 1){
+        this.aleatorio -= 1;
+      }
+    }
+  }
+
+  elegir(): void{
+    let diferencia = (this.oculto - this.aleatorio) * 100;
+
+    if(this.activado == true){
+      this.oro += Math.abs(diferencia);
+      this.activado = false;
+    }
+  }
+
+  pedirPrestamo(): void{
+    this.oro += 200;
+    this.deuda += 200;
+  }
+
+  pagarDeuda(): void{
+    if(this.oro >= this.deuda){
+      this.oro -= this.deuda;
+      this.deuda = 0;
+    } else {
+      alert('No te alcanza');
+    }
+  }
+  
 }
